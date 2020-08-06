d9511e0d2fd659b3e85f9c6c81383c8cdbdd6253
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-06 16:06:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song 
   attr_accessor :name, :artist
   
-  def initialize(name= "Ocean Eyes", artist="Billie")
+  def initialize(title= "Ocean Eyes", artist="Billie")
     @name = name
     @artist = artist
   end 
