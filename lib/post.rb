526069fba35d599d8cf2b4088d2bd8db51d05fee
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-06 16:04:54 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
   attr_accessor :name, :author
   
-  def initialize(title= "James", author =)
+  def initialize(title= "James", author = )
     @title = title
     @author = author
   end 
